48fda732134598ab4c85e87439860f968094abc2
Dziarson99 <134621120+Dziarson99@users.noreply.github.com>
2023-05-25 15:54:11 +0200

    Add files via upload

---
+++ APKUNIA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DVDLibraryApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            DVDLibrary library = new DVDLibrary();
+
+            library.AddDVD("The Shawshank Redemption");
+            library.AddDVD("The Godfather");
+            library.AddDVD("The Dark Knight");
+            library.AddDVD("12 Angry Men");
+
+            Console.WriteLine("Zaloguj się - jeśli jesteś pracownikiem, jeśli jesteś klientem, kliknij Enter");
+            string userType = Console.ReadLine();
+
+            if (userType == "admin")
+            {
+                AdminController.Run(library);
+            }
+            else
+            {
+                UserController.Run(library);
+            }
+        }
+    }
+}
